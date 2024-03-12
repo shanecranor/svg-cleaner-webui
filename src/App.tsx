55a7e7fc5ae502f64ae157745eae330a2355deb3
shanecranor <shanecranor@gmail.com>
2024-03-11 22:07:25 -0500

    toggle switches for each property

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import { useEffect, useState } from "react";
 
 import "./App.css";
-import { Button, Group, TextInput, useMantineColorScheme } from "@mantine/core";
+import {
+  Button,
+  Group,
+  Switch,
+  TextInput,
+  useMantineColorScheme,
+} from "@mantine/core";
 import init, {
   InitOutput,
   greet,
   clean_svg,
 } from "../public/wasm/svg_cleaner_wasm_wrapper.js";
 import { useInputState } from "@mantine/hooks";
+import { observer, useObservable } from "@legendapp/state/react";
 
-function App() {
+const App = observer(() => {
   const { setColorScheme } = useMantineColorScheme();
   const [inputSvgCode, setInputSvgCode] = useInputState("");
   const [wasmModule, setWasmModule] = useState<InitOutput | null>(null);
   const [cleanSvgCode, setCleanSvgCode] = useState<string>("");
-  const cleaningOptions: CleaningOptions = {
-    remove_unused_defs: true,
-    convert_shapes: true,
-    remove_title: true,
+  const cleaningOptions = useObservable({
+    remove_unused_defs: false,
+    convert_shapes: false,
+    remove_title: false,
     remove_desc: true,
     remove_metadata: true,
     remove_dupl_linear_gradients: true,
@@ -30,17 +37,17 @@ function App() {
     regroup_gradient_stops: true,
     remove_invalid_stops: true,
     remove_invisible_elements: true,
-    resolve_use: true,
+    resolve_use: false,
     remove_version: true,
     remove_unreferenced_ids: true,
-    trim_ids: true,
+    trim_ids: false, //dangerous?
     remove_text_attributes: true,
     remove_unused_coordinates: true,
     remove_default_attributes: true,
     remove_xmlns_xlink_attribute: true,
     remove_needless_attributes: true,
     remove_gradient_attributes: true,
-    join_style_attributes: 0,
+    join_style_attributes: 1,
     apply_transform_to_gradients: true,
     apply_transform_to_shapes: true,
     paths_to_relative: true,
@@ -48,11 +55,12 @@ function App() {
     convert_segments: true,
     append_newline: true,
     apply_transform_to_paths: true,
-    coordinates_precision: 2,
-    properties_precision: 2,
-    paths_coordinates_precision: 2,
-    transforms_precision: 2,
-  };
+    coordinates_precision: 1,
+    properties_precision: 1,
+    paths_coordinates_precision: 1,
+    transforms_precision: 1,
+  });
+
   useEffect(() => {
     const loadWasm = async () => {
       try {
@@ -85,10 +93,49 @@ function App() {
       >
         Clean SVG
       </Button>
-      <pre>{cleanSvgCode}</pre>
+      {Object.keys(cleaningOptions.get()).map((key) => {
+        const value = cleaningOptions.get()[key as keyof CleaningOptions];
+        let valString = "";
+        if (typeof value === "boolean") {
+          valString = value ? "true" : "false";
+          return (
+            <div key={key}>
+              <Switch
+                checked={Boolean(value)}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  console.log("fuck");
+                  cleaningOptions.set((prev) => ({
+                    ...prev,
+                    [key as keyof CleaningOptions]: event.currentTarget.checked,
+                  }));
+                }}
+              />
+              {key}: {valString}
+            </div>
+          );
+        }
+        if (typeof value === "number") {
+          valString = value.toString();
+        }
+        return (
+          <div key={key}>
+            <Switch
+              checked={value}
+              onChange={(checked) => {
+                cleaningOptions.set({ [key]: checked });
+              }}
+            />
+            {key}: {valString}
+          </div>
+        );
+      })}
+      {/* <pre>{cleanSvgCode}</pre> */}
+
+      <div dangerouslySetInnerHTML={{ __html: cleanSvgCode }} />
+      <div dangerouslySetInnerHTML={{ __html: inputSvgCode }} />
     </>
   );
-}
+});
 
 interface CleaningOptions {
   remove_unused_defs: boolean;
